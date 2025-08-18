c5c8e43bb3c8ad0190a53dc5a27affbb7e0626ac
Goopey <mega1134227@gmail.com>
2025-08-18 00:54:32 -0400

    Fixed a bug with items.

--- src/main/java/com/goopey/voidsentflame/core/init/ItemInit.java
+++ src/main/java/com/goopey/voidsentflame/core/init/ItemInit.java
@@ -2,15 +2,20 @@ package com.goopey.voidsentflame.core.init;
 
 import com.goopey.voidsentflame.VoidsentFlameMod;
 
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ItemInit {
   public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(VoidsentFlameMod.MODID);
 
-  public static final DeferredItem<Item> RUNIC_FRUIT = ITEMS.register("runic_fruit", () -> new Item(new Item.Properties()));
+  public static final DeferredItem<Item> RUNIC_FRUIT = ITEMS.registerItem("runic_fruit", iProperties -> new Item(iProperties), new Item.Properties());
 }
