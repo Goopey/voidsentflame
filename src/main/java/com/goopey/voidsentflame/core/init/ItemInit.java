4b263b89d6a5fa6250eb73d922f749fea1d35e67
Goopey <mega1134227@gmail.com>
2025-08-26 18:12:01 -0400

    Change RUBICON_IGNITER_ITEM itemclass to RubiconIgniterItem

--- src/main/java/com/goopey/voidsentflame/core/init/ItemInit.java
+++ src/main/java/com/goopey/voidsentflame/core/init/ItemInit.java
@@ -1,24 +1,16 @@
 package com.goopey.voidsentflame.core.init;
 
 import com.goopey.voidsentflame.VoidsentFlameMod;
+import com.goopey.voidsentflame.item.RubiconIgniterItem;
 
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class ItemInit {
   public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(VoidsentFlameMod.MODID);
 
   public static final DeferredItem<Item> RUNIC_FRUIT_ITEM = ITEMS.registerItem("runic_fruit_item", iProperties -> new Item(iProperties), new Item.Properties());
 
-  public static final DeferredItem<Item> RUBICON_IGNITER_ITEM = ITEMS.registerItem("rubicon_igniter_item", iProperties -> new Item(iProperties), new Item.Properties());
+  public static final DeferredItem<RubiconIgniterItem> RUBICON_IGNITER_ITEM = ITEMS.registerItem("rubicon_igniter_item", iProperties -> new RubiconIgniterItem(iProperties), new Item.Properties().stacksTo(1));
 }
--- src/main/java/com/goopey/voidsentflame/item/RubiconIgniterItem.java
+++ src/main/java/com/goopey/voidsentflame/item/RubiconIgniterItem.java
@@ -1,22 +1,14 @@
 package com.goopey.voidsentflame.item;
 
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.api.distmarker.Dist;
-
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
 
 import com.goopey.voidsentflame.block.RubiconPortalBlock;
-
-import java.util.List;
 
 import javax.annotation.Nonnull;
 
@@ -36,13 +28,9 @@ public class RubiconIgniterItem extends Item {
 		if (!entity.mayUseItemAt(pos, context.getClickedFace(), itemstack)) {
 			return InteractionResult.FAIL;
 		} else {
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
 			boolean success = false;
 			if (world.isEmptyBlock(pos) && true) {
 				RubiconPortalBlock.portalSpawn(world, pos);
-				itemstack.hurtAndBreak(1, entity, LivingEntity.getSlotForHand(context.getHand()));
 				success = true;
 			}
 			return success ? InteractionResult.SUCCESS : InteractionResult.FAIL;
