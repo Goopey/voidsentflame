946157ce3acc66ecea0dbcae67fd329e3793e8e7
Goopey <mega1134227@gmail.com>
2025-10-10 11:57:28 -0400

    Commented out VoidSeaLayerBlockEntityRenderer render

--- src/main/java/com/goopey/voidsentflame/client/render/VoidSeaLayerBlockEntityRenderer.java
+++ src/main/java/com/goopey/voidsentflame/client/render/VoidSeaLayerBlockEntityRenderer.java
@@ -32,11 +32,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.client.event.ContainerScreenEvent.Render;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.RenderStateShard.LightmapStateShard;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 
 public class VoidSeaLayerBlockEntityRenderer implements BlockEntityRenderer<VoidSeaLayerBlockEntity> {
@@ -54,6 +57,7 @@ public class VoidSeaLayerBlockEntityRenderer implements BlockEntityRenderer<Void
   private static int RENDER_DISTANCE = 128;
   
   // Shader Stuff
+  private RenderType EXAMPLE_RENDER;
   private static String GPU_TEXTURE_NAME = "void_waves";
   private GpuDevice gpu;
   private RenderPipeline distortPipeline;
@@ -66,9 +70,17 @@ public class VoidSeaLayerBlockEntityRenderer implements BlockEntityRenderer<Void
     Function<ResourceLocation, TextureAtlasSprite> atlas = Minecraft.getInstance().getTextureAtlas(atlasLocation);
     this.SPRITE = atlas.apply(res);
 
-    this.gpu = RenderSystem.getDevice();
-    setupPipelines();
-    setupQuad();
+    // this.gpu = RenderSystem.getDevice();
+    // setupPipelines();
+    // setupQuad();
+
+    // this.EXAMPLE_RENDER = RenderType.create(
+    //   "example:example", 
+    //   4194304, 
+    //   false, 
+    //   true, 
+    //   RenderPipelines.DRAGON_RAYS, 
+    //   RenderType.CompositeState.builder().setLightmapState(LightmapStateShard.LIGHTMAP).setTextureState(RenderStateShard.BLOCK_SHEET_MIPPED).createCompositeState(true)); 
   }
 
   //#####################################################
@@ -109,80 +121,81 @@ public class VoidSeaLayerBlockEntityRenderer implements BlockEntityRenderer<Void
   
   @Override
   public void render(@Nonnull VoidSeaLayerBlockEntity blockEntity, float partialTick, @Nonnull PoseStack poseStack, @Nonnull MultiBufferSource bufferSource, int packedLight, int packedOverlay, @Nonnull Vec3 cameraPos) {    
-    Map<Long, Float> heightCache = new HashMap<>();
-    
-    long gameTime = Minecraft.getInstance().level.getGameTime();
-    double t = gameTime + partialTick;
-    
-    BlockPos basePos = blockEntity.getBlockPos();
-    RenderType renderType = RenderType.solid();
-    VertexConsumer builder = bufferSource.getBuffer(renderType);
+    // Map<Long, Float> heightCache = new HashMap<>();
 
-    poseStack.pushPose();
-
-    // set height to 5 blocks
-    poseStack.translate(0, HEIGHT, 0);
-
-    Matrix4f mat = poseStack.last().pose();
-    PoseStack.Pose pose = poseStack.last();
+    // // VoidsentFlameMod.LOGGER.info("Packed Light:["+packedLight+"]      Packed Overlay:["+packedOverlay+"]");
     
-    // iterate across grid
-    for (int ix = 0; ix < VoidSeaLayerBlockEntity.WIDTH; ix++) {
-      for (int iz = 0; iz < VoidSeaLayerBlockEntity.LENGTH; iz++) {
-        // inside each block, subdivide
-        for (int sx = 0; sx < SUBDIV; sx++) {
-          for (int sz = 0; sz < SUBDIV; sz++) {
-            // compute fractional positions 0..1
-            float x0 = ix + (float)sx / SUBDIV;
-            float z0 = iz + (float)sz / SUBDIV;
-            float x1 = ix + (float)(sx+1) / SUBDIV;
-            float z1 = iz + (float)(sz+1) / SUBDIV;
-            
-            // world positions for wave sample
-            double wx0 = basePos.getX() + x0;
-            double wz0 = basePos.getZ() + z0;
-            double wx1 = basePos.getX() + x1;
-            double wz1 = basePos.getZ() + z0;
-            double wx2 = basePos.getX() + x1;
-            double wz2 = basePos.getZ() + z1;
-            double wx3 = basePos.getX() + x0;
-            double wz3 = basePos.getZ() + z1;
-            
-            // heights = sin wave
-            float h0 = getCachedHeight(wx0, wz0, t, heightCache);
-            float h1 = getCachedHeight(wx1, wz1, t, heightCache);
-            float h2 = getCachedHeight(wx2, wz2, t, heightCache);
-            float h3 = getCachedHeight(wx3, wz3, t, heightCache);
-            
-            // compute UV coords (u0,v0 etc)
-            float u0 = this.SPRITE.getU0();
-            float v0 = this.SPRITE.getV0();
-            float u1 = this.SPRITE.getU1();
-            float v1 = this.SPRITE.getV1();
-            
-            // top surface
-            putVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
-            putVertex(builder, mat, x0, h3, z1, u0, v1, packedLight, packedOverlay, pose);
-            putVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
-            
-            putVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
-            putVertex(builder, mat, x1, h1, z0, u1, v0, packedLight, packedOverlay, pose);
-            putVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
-            
-            // bottom surface
-            reversePutVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
-            reversePutVertex(builder, mat, x0, h3, z1, u0, v1, packedLight, packedOverlay, pose);
-            reversePutVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
-            
-            reversePutVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
-            reversePutVertex(builder, mat, x1, h1, z0, u1, v0, packedLight, packedOverlay, pose);
-            reversePutVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
-          }
-        }
-      }
-    };
+    // long gameTime = Minecraft.getInstance().level.getGameTime();
+    // double t = gameTime + partialTick;
     
-    poseStack.popPose();
+    // BlockPos basePos = blockEntity.getBlockPos();
+    // VertexConsumer builder = bufferSource.getBuffer(this.EXAMPLE_RENDER);
+
+    // poseStack.pushPose();
+
+    // // set height to 5 blocks
+    // poseStack.translate(0, HEIGHT, 0);
+
+    // Matrix4f mat = poseStack.last().pose();
+    // PoseStack.Pose pose = poseStack.last();
+    
+    // // iterate across grid
+    // for (int ix = 0; ix < VoidSeaLayerBlockEntity.WIDTH; ix++) {
+    //   for (int iz = 0; iz < VoidSeaLayerBlockEntity.LENGTH; iz++) {
+    //     // inside each block, subdivide
+    //     for (int sx = 0; sx < SUBDIV; sx++) {
+    //       for (int sz = 0; sz < SUBDIV; sz++) {
+    //         // compute fractional positions 0..1
+    //         float x0 = ix + (float)sx / SUBDIV;
+    //         float z0 = iz + (float)sz / SUBDIV;
+    //         float x1 = ix + (float)(sx+1) / SUBDIV;
+    //         float z1 = iz + (float)(sz+1) / SUBDIV;
+            
+    //         // world positions for wave sample
+    //         double wx0 = basePos.getX() + x0;
+    //         double wz0 = basePos.getZ() + z0;
+    //         double wx1 = basePos.getX() + x1;
+    //         double wz1 = basePos.getZ() + z0;
+    //         double wx2 = basePos.getX() + x1;
+    //         double wz2 = basePos.getZ() + z1;
+    //         double wx3 = basePos.getX() + x0;
+    //         double wz3 = basePos.getZ() + z1;
+            
+    //         // heights = sin wave
+    //         float h0 = getCachedHeight(wx0, wz0, t, heightCache);
+    //         float h1 = getCachedHeight(wx1, wz1, t, heightCache);
+    //         float h2 = getCachedHeight(wx2, wz2, t, heightCache);
+    //         float h3 = getCachedHeight(wx3, wz3, t, heightCache);
+            
+    //         // compute UV coords (u0,v0 etc)
+    //         float u0 = this.SPRITE.getU0();
+    //         float v0 = this.SPRITE.getV0();
+    //         float u1 = this.SPRITE.getU1();
+    //         float v1 = this.SPRITE.getV1();
+            
+    //         // top surface
+    //         putVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
+    //         putVertex(builder, mat, x0, h3, z1, u0, v1, packedLight, packedOverlay, pose);
+    //         putVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
+            
+    //         putVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
+    //         putVertex(builder, mat, x1, h1, z0, u1, v0, packedLight, packedOverlay, pose);
+    //         putVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
+            
+    //         // bottom surface
+    //         reversePutVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
+    //         reversePutVertex(builder, mat, x0, h3, z1, u0, v1, packedLight, packedOverlay, pose);
+    //         reversePutVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
+            
+    //         reversePutVertex(builder, mat, x1, h2, z1, u1, v1, packedLight, packedOverlay, pose);
+    //         reversePutVertex(builder, mat, x1, h1, z0, u1, v0, packedLight, packedOverlay, pose);
+    //         reversePutVertex(builder, mat, x0, h0, z0, u0, v0, packedLight, packedOverlay, pose);
+    //       }
+    //     }
+    //   }
+    // };
+    
+    // poseStack.popPose();
   }
   
   /**
